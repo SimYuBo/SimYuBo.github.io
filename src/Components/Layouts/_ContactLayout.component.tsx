12d8c2175723ad112f836d3e20306f9af20098bb
SimYuBo <21024095@myrp.edu.sg>
2023-05-17 14:45:15 +0800

    Added missing fade to contact

--- src/Components/Layouts/_ContactLayout.component.tsx
+++ src/Components/Layouts/_ContactLayout.component.tsx
@@ -1,6 +1,6 @@
 import { Box, IconButton } from "@mui/material";
 import ContactForm from "../ContactPage/CustomForm";
-import { LightHeader, LightText, RootBox, TopMarginBox } from "../Miscellaneous/GlobalElements.component";
+import { FadeInSection, LightHeader, LightText, RootBox, TopMarginBox } from "../Miscellaneous/GlobalElements.component";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { IconBox, IconSx } from "../../Styles/ContactPage/ContactStyles";
@@ -11,15 +11,19 @@ export default function ContactLayout() {
         <>
             <RootBox>
                 <TopMarginBox>
-                    <LightHeader>
-                        How would I reach you?
-                    </LightHeader>
-                    <LightText>
-                        Are you interested in working with me? Drop me a message using the form below.
-                    </LightText>
+                    <FadeInSection>
+                        <LightHeader>
+                            How would I reach you?
+                        </LightHeader>
+                        <LightText>
+                            Are you interested in working with me? Drop me a message using the form below.
+                        </LightText>
+                    </FadeInSection>
                 </TopMarginBox>
-                <ContactForm />
-                
+                <FadeInSection>
+                    <ContactForm />
+                </FadeInSection>
+                <FadeInSection>
                     <IconBox>
                         <IconButton
                             sx={IconSx}
@@ -42,6 +46,7 @@ export default function ContactLayout() {
                             <LinkedInIcon />
                         </IconButton>
                     </IconBox>
+                </FadeInSection>
                 <Footer />
             </RootBox>
         </>
